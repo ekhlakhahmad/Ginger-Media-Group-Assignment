f85e09f4a54d9f14920bb32cf8ee79f1435f2a67
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-02-10 15:53:58 +0530

    ginger media assignment

---
+++ Ginger_Ekh/README.md
@@ -0,0 +1,116 @@
+<h2 align='center'>Ginger Media Assignment 📃</h2>
+<p align="center">
+<a href="https://github.com/ekhlakhahmad/"><img title="Author" src="https://img.shields.io/badge/Author-ekhlakhahmad--red.svg?style=for-the-badge&logo=github"></a>
+</p>
+
+<p align="center">
+<a href="https://github.com/ekhlakhahmad"><img title="Followers" src="https://img.shields.io/github/followers/ekhlakhahmad?color=teal&style=flat-square"></a>
+<a href="https://github.com/ekhlakhahmad/Ginger-Media-Group-Assignment.git/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/ekhlakhahmad/Ginger-Media-Group-Assignment.gitt?color=brown&style=flat-square"></a>
+<a href="https://github.com/ekhlakhahmad/Ginger-Media-Group-Assignment.git/network/members"><img title="Forks" src="https://img.shields.io/github/forks/Rahulkrsin/GingerMediaGroup_Assignment?color=lightgrey&style=flat-square"></a>
+<a href="https://github.com/ekhlakhahmad/Ginger-Media-Group-Assignment.git/issues"><img title="issues" src="https://img.shields.io/github/issues/ekhlakhahmad/Ginger-Media-Group-Assignment.git?style=flat-square">
+</a>
+
+</p>
+
+<p align="center">
+    Ginger Media Assignment
+</p>
+
+## live-link 🔗
+
+<a target="_blank" href="https://ginger-media-yz0x.onrender.com/"><b>https://ginger-media-yz0x.onrender.com/</b></a>
+
+# main features
+
+- register a user
+- login a user
+- logout a user
+- show profile page
+- update profile
+
+# Demo Creadential
+
+- email : - `ekhlakhahmadpc@gmail.com`
+- password: - `Test@123`
+
+# tech stack used
+
+Fronted
+
+- html, css, js, React
+
+Backend
+
+- nodejs, express
+
+Database
+
+- MySql
+
+## Installation
+
+```sh
+
+# Clone the repo
+$ git clone git remote add origin https://github.com/ekhlakhahmad/Ginger-Media-Group-Assignment.git
+
+# Install
+$ npm install
+
+# Setting Up ENV
+> setup .env file for database
+
+
+
+# Start
+$ npm start
+
+# Access your app
+$ http://localhost:${PORT}
+
+```
+
+## Example
+
+### register a user
+
+<br/>
+<div align="center">
+  <img  src="./example/signup.png" />
+</div>
+<br/>
+<br/>
+
+### login a user
+
+<div align="center">
+  <img  src="./example/singin.png" />
+</div>
+<br/>
+
+### main Dashboard
+
+<div align="center">
+  <img  src="./example/profile.png" />
+</div>
+<br/>
+
+### update profile
+
+<div align="center">
+  <img  src="./example/update_profile.png" />
+</div>
+<br/>
+
+## Author
+
+👤 **Ekhlakh Ahmad**
+
+- Github: [@ekhlakhahmad](https://github.com/ekhlakhahmad/)
+- LinkedIN: [@Ekhlakh Ahmad](https://www.linkedin.com/in/ekhlakhahmad/)
+
+---
+
+## License
+
+&copy; Ekhlakh Ahmad | MIT
---
+++ Ginger_Ekh/frontend/src/Pages/Login.jsx
@@ -0,0 +1,141 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import useInputFocus from "../Components/useInputFocus";
+import authentication from "../assets/authentication.svg";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useInputFocus();
+
+  const [formData, setFormData] = useState({
+    email: "",
+
+    password: "",
+  });
+
+  const { isLoading, isError, user, msg } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(msg);
+    }
+
+    if (user) {
+      toast.success("Login successful!");
+      navigate("/");
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [isError, user, msg, dispatch, navigate]);
+
+  const { email, password } = formData;
+
+  //geting inputed data when change happen
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  //handlesubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      toast.error("all field is required");
+    } else {
+      dispatch(login(formData));
+    }
+  };
+
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+  return (
+    <>
+      <div className="l-form">
+        <div className="shape1"></div>
+        <div className="shape2"></div>
+
+        <div className="form">
+          <img src={authentication} alt="" className="form__img" />
+
+          <form action="" className="form__content" onSubmit={handleSubmit}>
+            <h1 className="form__title">Welcome</h1>
+
+            <div className="form__div form__div-one">
+              <div className="form__icon">
+                <i className="bx bx-user-circle"></i>
+              </div>
+
+              <div className="form__div-input">
+                <label htmlFor="username" className="form__label">
+                  Gmail/Number
+                </label>
+                <input
+                  type="text"
+                  className="form__input"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="form__div">
+              <div className="form__icon">
+                <i className="bx bx-lock"></i>
+              </div>
+
+              <div className="form__div-input">
+                <label htmlFor="password" className="form__label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form__input"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <a href="#" className="form__forgot">
+              Forgot Password?
+            </a>
+
+            <input type="submit" className="form__button" value="Login" />
+            <p className="signup-link">
+              Create a new account <Link to={"/signup"}>Sign Up</Link>
+            </p>
+
+            <div className="form__social">
+              <span className="form__social-text">Our login with</span>
+
+              <a href="#" className="form__social-icon">
+                <i className="bx bxl-facebook"></i>
+              </a>
+              <a href="#" className="form__social-icon">
+                <i className="bx bxl-google"></i>
+              </a>
+              <a href="#" className="form__social-icon">
+                <i className="bx bxl-instagram"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
